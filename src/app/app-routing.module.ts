a6a6be00db3c63e30b1ef1f02c5f0c421d7e74f1
JABC <jefersonbc000@gmail.com>
2021-03-19 06:31:54 -0400

    AuthGuard Implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+const redirectToLoggedUser = () => redirectLoggedInTo(['app']);
 
 const routes: Routes = [
   { path: '', redirectTo: 'app', pathMatch: 'full' },
   {
     path: 'auth',
-    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule),
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectToLoggedUser }
   },
   {
     path: 'app',
